54dd28508bc5ffc1d9744ed652c588268d2b7726
Andres Caicedo <caicedoleon.andresfelipe@gmail.com>
2022-08-25 14:58:41 -0500

    08 added

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,9 +6,16 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length<=1)return array;
+  var pivot = [array[0]];
+  var der = [];
+  var izq = [];
+  for(var i = 0; i<array.length; i++){
+    if(array[i]>pivot[0]) der.push(array[i]);
+    if(array[i]<pivot[0]) izq.push(array[i]);
+  }
+  return quickSort(izq).concat(pivot).concat(quickSort(der));
 }
-
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
