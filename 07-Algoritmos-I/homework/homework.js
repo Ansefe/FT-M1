d48f3a2b9f5c55dd4c2f3ddfa09fad5d3274865f
Andres Caicedo <caicedoleon.andresfelipe@gmail.com>
2022-08-25 13:48:27 -0500

    07 completed

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,21 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var i = 1;
+  const arr = [];
+  while(i>0){
+    if(num%i==0){
+      arr.push(i);
+      num = num/i;
+      i = 1;
+    }
+    if(num==1){
+      i=0;
+    }else{
+      i++;
+    }
+  }
+  return arr;
 }
 
 function bubbleSort(array) {
@@ -14,16 +28,37 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var aux = null;
+  for(var i=0; i<array.length; i++){
+    if(array[i]>array[i+1]){
+      aux = array[i];
+      array[i] = array[i+1];
+      array[i+1] = aux;
+    }
+  }
+  if(aux){
+    bubbleSort(array);
+  }
+  return(array);
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(var i = 1; i<array.length;i++){
+    var aux = 0;
+    for(var j = i-1; j >= 0; j--){
+      if(array[i]<array[j]){
+        aux = array[j];
+        array[j] = array[i];
+        array[i] = aux;
+        i--;
+      }      
+    }
+  }
+  return array;
 }
 
 
@@ -32,10 +67,30 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  const pos = 1;
+  for(var j=0; j<array.length;j++){
+    var aux = array[j];
+    var aux2 = 0;
+    var minIdx = 0;
+    for(var i = j+1;i<array.length;i++){
+      if(array[i]<aux){
+        aux = array[i];
+        minIdx = i;
+      }
+    }
+    for(var k=minIdx-1; k>=0; k--){
+      if(array[minIdx]<array[k]){
+        aux2 = array[k];
+        array[k] = array[minIdx];
+        array[minIdx] = aux2;
+        minIdx--;
+      }
+    }
+  }
+  return array;
 }
 
-
+console.log(selectionSort([5,2,3,1,0,4]));
 // No modificar nada debajo de esta línea
 // --------------------------------
 
